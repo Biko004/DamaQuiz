d652f62bbf687365456ce5be0ecb4ff6690b28ab
biko004 <Biko004@gmail.com>
2016-12-18 11:48:05 +0200

    fixing files

--- js/hideandshow.js
+++ js/hideandshow.js
@@ -21,16 +21,16 @@ var  answerCheckDropDown = function(){
     if(answerValue == "select"){
         alert("Please select your answer");
     }
-    else if(parseInt(answerValue) == "1"){
+    else if(parseInt(answerValue) == 1){
         score+= 1;
     }
-    else if(parseInt(answerValue) == "2"){
+    else if(parseInt(answerValue) == 2){
         score+= 2;
     }
-    else if(parseInt(answerValue) == "3"){
+    else if(parseInt(answerValue) == 3){
         score+= 3;
     }
-    else if(parseInt(answerValue) == "4"){
+    else if(parseInt(answerValue) == 4){
         score+= 4;
     }
     console.log(score);
@@ -109,5 +109,5 @@ function hideAndShow5(){
         document.getElementById("winner").style.display = 'block';
         document.getElementById("five").style.display = 'none';
     }
-alert("Your score is " + score)
+
 }
